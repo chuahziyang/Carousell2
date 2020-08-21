3450247dd8b015073330c1f1feaa0e4b58cc8b10
czyan <czyang2002@gmail,com>
2020-08-21 12:46:33 +0800

    sex with celestine

--- venv/Project/Run.py
+++ venv/Project/Run.py
@@ -2,7 +2,11 @@ import json
 import re
 import os
 
+import requests
 
+def remove(string):
+    return string.translate(str.maketrans({"\\": r"\\",
+                                           "/": ""}))
 
 class product():
     def __init__(self, Description, Title,Price, link):
@@ -12,17 +16,29 @@ class product():
         links = []
         for i in link:
             match = re.search("&quot;(.*tn&quot)",i["name"])
+            links.append(match.group(1))
         self.link = links
+        print(self.link)
 
 
-with open('run_results.json',encoding="utf8") as json_file:
+with open('run_results_new.json',encoding="utf8") as json_file:
     data = json.load(json_file)
     print(data)
 
     os.chdir("../Files")
 
     for i in data['list1']:
-        item = product(i["Description"],"Placeholder", i["selection2"], i["selection1"])
-        os.mkdir(item.title)
+        item = product(i["Description"],i["Title"], i["selection2"], i["selection1"])
+        os.mkdir(remove(item.title))
+        print(os.getcwd())
+        os.chdir(remove(item.title))
+        i = 0
         for image in item.link:
-            pass
+            print(image)
+            r = requests.get(image, allow_redirects=True)
+            open(str(i), 'wb').write(r.content)
+            i += 1
+
+        os.chdir("../")
+        print(os.getcwd())
+
